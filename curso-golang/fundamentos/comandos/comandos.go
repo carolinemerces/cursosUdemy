e8de58bc85c5ab21ebd794de9dcc9031c6dd8287
Caroline das Mercês <carolinedasmerces@gmail.com>
2021-02-08 12:01:47 -0300

    update comandos

--- curso-golang/fundamentos/comandos/comandos.go
+++ curso-golang/fundamentos/comandos/comandos.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Printf("Outro programa em %s!!!!!!!\n", "Go")
 }
+
+/*Para executar diretamente no terminal -> ./"nome do arquivo" ou
+go run "nome do arquivo"
+*/
+
+/*Instalando uma dependêndia a partir do comando get no terminal:
+go get -u "pasta da dependência a ser instalada"/"nome da pasta da dependência a ser instalada"/"denpendência a ser instalada"
+*/
+
+/*comandos importantes:
+go env (configuracoes)
+go version
+go get (instalacao de dependência)
+go help
+go run
+go build (constrói um outro arquivo idêntico)
+go vet (Vet examines Go source code and reports suspicious constructs)
+go doc
+*/
