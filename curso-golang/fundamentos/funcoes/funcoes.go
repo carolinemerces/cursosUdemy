baec46c682bc3a9a95a3157d01f89c9a30d5d4b0
Caroline das Mercês <carolinedasmerces@gmail.com>
2021-02-08 16:33:26 -0300

    funcoes

---
+++ curso-golang/fundamentos/funcoes/funcoes.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+//funcão = um conjunto de instrucões e comandos a serem executados (receita de bolo)
+func somar(a int, b int) int {
+	return a + b
+}
+
+func imprimir(valor int) {
+	fmt.Println(valor)
+}
+
+/*func main(){
+	resultado := somar(3, 4)
+	imprimir(resultado)
+}
+
+Comentado para testar a app com o arquivo main.go
+
+*/ 
---
+++ curso-golang/fundamentos/funcoes/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	resultado := somar(3, 4)
+	imprimir(resultado)
+}
+
+
+//No terminal para executar funcoes.go com o arquivo main.go usar -> go run *.go
+//Ele executa direto no terminal
