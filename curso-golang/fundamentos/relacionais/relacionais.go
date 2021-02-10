9c3b88515cb4291aa6fcfe7b9edf8e614cdbb62e
Caroline das Mercês <carolinedasmerces@gmail.com>
2021-02-10 10:16:57 -0300

    relacionais

--- curso-golang/fundamentos/relacionais/relacionais.go
+++ curso-golang/fundamentos/relacionais/relacionais.go
@@ -13,12 +13,14 @@ func main() {
 	fmt.Println("<=", 3 <= 2)
 	fmt.Println(">=", 3 >= 2)
 
+	//datas
 	d1 := time.Unix(0, 0)
 	d2 := time.Unix(0, 0)
 
 	fmt.Println("Datas:", d1 == d2)
 	fmt.Println("Datas:", d1.Equal(d2))
 
+	//struct
 	type Pessoa struct {
 		Nome string
 	}
