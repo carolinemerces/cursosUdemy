1d90e0675de56f3e84b01121a161dda54e68a49e
Caroline das Mercês <carolinedasmerces@gmail.com>
2021-02-10 09:50:12 -0300

    unários

---
+++ curso-golang/fundamentos/unarios/unarios.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 1
+	y := 2
+
+	//valor de incremento ou decremento apenas pós fixada, vem depois do operando
+	x++ //x += 1 ou x = x+1
+	y-- //y -= 1 ou y = y-1
+	fmt.Println(x, y)
+}
