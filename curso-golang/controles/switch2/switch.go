4085c80a819907bfe5594260aba46cec9c594cda
Caroline das Mercês <carolinedasmerces@gmail.com>
2021-02-10 16:26:00 -0300

    switch2

--- curso-golang/controles/switch2/switch.go
+++ curso-golang/controles/switch2/switch.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	t := time.Now() //hora atual
-	switch true {   //switch true e ele vai procurar o primeiro case que seja verdadeiro
+	switch /*true*/{   //switch true e ele vai procurar o primeiro case que seja verdadeiro
 	case t.Hour() < 12:
 		fmt.Println("Bom dia!")
 	case t.Hour() < 18:
